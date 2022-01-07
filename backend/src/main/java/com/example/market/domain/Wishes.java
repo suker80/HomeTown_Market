8733be6dd5671556389f0c0718e91367c3d3ccf0
kkjiyeon <91598037+kkjiyeon@users.noreply.github.com>
2022-01-07 22:28:09 +0900

    Update Wishes.java

--- backend/src/main/java/com/example/market/domain/Wishes.java
+++ backend/src/main/java/com/example/market/domain/Wishes.java
@@ -18,7 +18,7 @@ public class Wishes {
     @JoinColumn(name = "product_id")
     private Product product;
     
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+//     @ManyToOne
+//     @JoinColumn(name = "user_id")
+//     private User user;
 }
